48beaeef9f54385af33a2ddf978d702f4489ae56
sovanda_m <vanda.can.read@gmail.com>
2025-03-31 23:57:22 +0700

    Add name

--- src/main/java/me/itc/App.java
+++ src/main/java/me/itc/App.java
@@ -4,10 +4,9 @@ package me.itc;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("MITH Sovanda");
     }
 }
