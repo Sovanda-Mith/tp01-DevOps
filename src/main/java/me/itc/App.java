d2eeeb37252b0596a4ddb07e4238be19c5939194
sovanda_m <vanda.can.read@gmail.com>
2025-04-01 11:42:05 +0700

    println

--- src/main/java/me/itc/App.java
+++ src/main/java/me/itc/App.java
@@ -5,8 +5,9 @@ package me.itc;
  *
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("MITH Sovanda");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("MITH Sovanda");
+    System.out.println("Testing");
+  }
 }
